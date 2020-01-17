4446c50f5cf6b4cf1e6abe055f2332819b3c6504
gpla <germanps@outlook.es>
2020-01-17 12:33:19 +0100

    first commit

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+
+    return(
+        <h1>desde NuevaCuenta</h1>
+    );
+}
+
+export default NuevaCuenta;
---
+++ src/components/proyectos/Proyectos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Proyectos = () => {
+
+    return(
+        <h1>desde proyectos</h1>
+    );
+}
+
+export default Proyectos;
