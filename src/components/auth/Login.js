4aaecde6556caeb27c360c020571ff58ddeb5495
gpla <germanps@outlook.es>
2020-01-17 13:07:01 +0100

    creación del formulario para el login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,8 +2,45 @@ import React from 'react';
 
 const Login = () => {
 
+    const _handleChange = () => {
+
+    }
+
     return(
-        <h1>desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar sesión</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            onChange={_handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={_handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar sesión"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
