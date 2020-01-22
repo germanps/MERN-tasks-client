b0ea0b7ca07f15cf57d7f547c134c3850ea40d47
germanps <gpla@pasiona.com>
2020-01-22 11:10:39 +0100

    componente tarea OK

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Tarea = () => {
+const Tarea = ({tarea}) => {
     return(
-        <li>
-            tarea
+        <li className="tarea sombra">
+            <div>{tarea.nombre}</div>
+            <div className="estado">
+                {tarea.estado
+                    ?   <button
+                            type="button"
+                            className="btn completo"
+                        >Completo</button>
+                    :   <button
+                            type="button"
+                            className="btn incompleto"
+                        >Incompleto</button>
+                }
+            </div>
+            <div className="acciones">
+                <button 
+                    type="button"
+                    className="btn bnt-primario"
+                >Eliminar</button>
+            </div>
         </li>
     )
 }
