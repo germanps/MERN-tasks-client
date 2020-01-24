4f4f0c32021cbbc73c90fc8c7cc6237a653a7bc6
gpla <germanps@outlook.es>
2020-01-24 09:01:06 +0100

    crear context, state y reducer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Proyectos from './components/proyectos/Proyectos';
 import Login from './components/auth/Login';
 import NuevaCuenta from './components/auth/NuevaCuenta';
+
+//Pasar los states al padre para que estén disponiblen en todos los hijos
 import ProyectoState from './context/proyectos/proyectoState';
+import TareaState from './context/tareas/tareaState';
 
 function App() {
   return (
     <div className="App">
       <ProyectoState>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
-            <Route exact path='/proyectos' component={Proyectos} />
-          </Switch>
-        </Router>
+        <TareaState>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
+              <Route exact path='/proyectos' component={Proyectos} />
+            </Switch>
+          </Router>
+        </TareaState>
       </ProyectoState>
     </div>
   );
